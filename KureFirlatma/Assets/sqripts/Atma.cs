f16e6137dabf30f1ee9a50fb3ba562d26e680d29
Abdulkadir DURMAZ <abdulkadirdurmaz52@gmail.com>
2023-07-20 16:50:50 +0300

    Küre Fırlatma
    Unity'de FPS oyunlarındaki silahların çalışma mekanizmasını öğrenmeye çalıştım.

---
+++ KureFirlatma/Assets/sqripts/Atma.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atma : MonoBehaviour
+{
+    
+    Rigidbody top;
+    public GameObject go;
+    public static int a;
+    bool sa;
+    // Start is called before the first frame update
+    void Start()
+    {
+       top = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0) && !sa)
+        {
+            //top.transform.position = new Vector3(top.transform.position.x, top.transform.position.y + a, top.transform.position.z + a);
+            //a += 1;
+            
+            Instantiate(go);
+            //sa = true;
+        }
+    }
+}
---
+++ KureFirlatma/Assets/sqripts/atma1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atma1 : MonoBehaviour
+{
+    bool a = true;
+    Rigidbody top;
+    // Start is called before the first frame update
+    void Start()
+    {
+        top = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (a)
+        {
+            top.GetComponent<Rigidbody>().AddForce(transform.forward * 100);
+            a = false;
+        }
+        
+    }
+}
